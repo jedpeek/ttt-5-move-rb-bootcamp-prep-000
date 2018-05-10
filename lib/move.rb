3f3eccfdc43c05b98a49ff56bdc90e6335fdbed1
Jed Peek <jedpeek@yahoo.com>
2018-05-10 17:15:00 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,9 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-index = (user_input.to_i)-1
+input = (user_input.to_i)-1
 end
-
+index = input_to_index(user_input)
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def move(array, index, value = "X")
   array[index] = value
